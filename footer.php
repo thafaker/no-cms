a68560b5cd41f3ad7e4495596c5e8fc7c8f09d8a
thafaker <thafaker@gmx.net>
2026-07-06 13:21:43 +0200

    rss in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div>$ uptime</div>
     <div style="color:#666; margin-top:0.5rem;">
-        © <?= date('Y') ?> Jan Montag <a href="https://github.com/thafaker/no-cms">NoCMS</a> · posts powered by flat files
+        © <?= date('Y') ?> Jan Montag <a href="https://github.com/thafaker/no-cms">NoCMS</a> · posts powered by flat files<br />
         <a href="/feed.xml">RSS</a>
     </div>
 </div>
